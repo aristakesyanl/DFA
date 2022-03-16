90f881d1d56e97d18d02ed60e56cc8961d38eaf1
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-03-16 20:12:12 +0400

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include"DFA.h"
+#include<fstream>
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<string>
+int main(int argc, char* argv[]) {
+    if (argc >= 1) {
+        cout << "No Argument Provided" << endl;
+    }
+    else {
+        vector<string> s;
+        for (int i = 1; i < argc; i++) {
+            s.push_back(argv[i]);
+        }
+
+        fstream fstrm;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            fstrm.open(s[i]);
+            if (fstrm.is_open()) {
+                DFA a(fstrm);
+            }
+            else {
+                cerr << "Failed to read text file" << endl;
+            }
+            fstrm.close();
+
+        }
+    }
+
+}
