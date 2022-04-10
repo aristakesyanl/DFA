9e6fb51826abfbd586b863d2f13ac0b98deb1882
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-04-10 08:38:34 +0400

    Update DFA.h

--- DFA.h
+++ DFA.h
@@ -1,32 +1,40 @@
 #include<string>
 #include<map>
-#include<fstream>
+#include<utility>
 
 
 /***********************************************************************
 * 
- * Header file defines a class of Deterministic Finite Automaton
+ * Header file defines a class of Turing Machine Simulator
  * It recerives an input text file, which contains an information about 
- * Deterministic Finite Automaton and configures such.
- * We can run the machine with the help of class method run(), which runs
- * corresponding string on the machine and return the result
+ * Turing Machine and configures such.
+ * We can run the machine in two modes: exit and debug.
+ * In exit mode the simulator prints the output of Turing Machine with the 
+ * given configuration. In debug mode the program prints the output of each 
+ * step after pressing Enter. To exit debug mode press Ctrl+Alt+D.
  * 
  * *********************************************************************/
-#ifndef DFA_H
-#define DFA_H
-class DFA {
+
+#ifndef TURING_H
+#define TURING_H
+class Turing {
 public:
-    DFA(std::string&);
+    Turing(std::string&);
     std::string run(std::string&);
+    std::string debug(std::string&, std::string&);
 private:
+    struct decision{
+        char symbol;
+        int state;
+        char direction; //right=r, left=l
+    }
     std::vector<char> inputSymbol; //set of input symbols
     std::set<char> sInput;
     std::vector<char> outputSymbol;//set of output symbols
     std::set<char> sOutput;
     std::vector<int> states;//set of internal states
     std::set<int> sStates;
-    std::map<std::pair<char, int>, char> writeSymbol;//function that returns symbol to be written in output string
-    std::map<std::pair<char, int>, int> changeState;//function that return the state the autamaton is going to change
+    std::map<std::pair<char,int>, decision> change;
     void parseInput(std::string);
     void parseOutput(std::string);
     void parseStates(std::string);
